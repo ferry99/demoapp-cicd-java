71cce8daa5ca05ec47f50e1eafc680eeabdf2ae6
Ferry Sanjaya <ferry@ELIFE.CO.ID>
2022-07-12 08:03:10 +0700

    change multiple step jenkins

---
+++ script.sh
@@ -0,0 +1,11 @@
+REPOSITORIES=`pwd`
+
+echo "Updating $REPOSITORIES at `date`"
+
+VERSION=`cat ./.env | grep VERSION= | cut -d '=' -f2`
+
+echo $VERSION
+
+docker-compose up -f docker-compose-prod.yml -d
+
+# docker run -d -p 8181:9999 ferry9999/demoapp:v1.0.0-M.4 /bin/bash -c 'java -jar demoapp-v1.0.0-M.4.jar'
--- src/main/java/com/example/kafkademo/controller/TestController.java
+++ src/main/java/com/example/kafkademo/controller/TestController.java
@@ -1,18 +1,49 @@
 package com.example.kafkademo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/app/demo")
 
 public class TestController {
 
-    @GetMapping(value = "")
-    public Object demo(){
-        return "Hello world";
+    @GetMapping(value = "" , produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String demo(){
+        return "<html>\n"
+                + "<header><title>Welcome</title></header>\n" +
+                "<body>\n" +
+                "<table>\n" +
+                        "  <tr>\n" +
+                        "    <th>Company</th>\n" +
+                        "    <th>Contact</th>\n" +
+                        "    <th>Country</th>\n" +
+                        "  </tr>\n" +
+                        "  <tr>\n" +
+                        "    <td>Alfreds Futterkiste</td>\n" +
+                        "    <td>Maria Anders</td>\n" +
+                        "    <td>Germany</td>\n" +
+                        "  </tr>\n" +
+                        "  <tr>\n" +
+                        "    <td>Centro comercial Moctezuma</td>\n" +
+                        "    <td>Francisco Chang</td>\n" +
+                        "    <td>Mexico</td>\n" +
+                        "  </tr>\n" +
+                        "  <tr>\n" +
+                        "    <td>Ernst Handel</td>\n" +
+                        "    <td>Roland Mendel</td>\n" +
+                        "    <td>Austria</td>\n" +
+                        "  </tr>\n" +
+                        "  <tr>\n" +
+                        "    <td>Island Trading</td>\n" +
+                        "    <td>Helen Bennett</td>\n" +
+                        "    <td>UK</td>\n" +
+                        "  </tr>\n" +
+                        "</table>\n"
+                + "</body>\n"
+                + "</html>";
     }
 
 }
